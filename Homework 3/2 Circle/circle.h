dfe756b447e951146bf79d04773e87e9102f25a8
nsbalint <nsbalintps@gmail.com>
2024-03-14 18:53:45 +0100

    Third week done and runnable

--- Homework 3/2 Circle/circle.h
+++ Homework 3/2 Circle/circle.h
@@ -1,9 +1,11 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
-/**
- * Data of a circle object in Descartes coordinate system
- */
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
 typedef struct Circle
 {
 	double x;
@@ -11,14 +13,7 @@ typedef struct Circle
 	double radius;
 } Circle;
 
-/**
- * Set the data of the circle
- */
-void set_circle_data(Circle* circle, double x, double y, double radius);
-
-/**
- * Calculate the area of the circle.
- */
-double calc_circle_area(const Circle* circle);
+void set_circle_data(Circle *circle, double x, double y, double radius, SDL_Color color);
+double calc_circle_area(const Circle *circle);
 
 #endif // CIRCLE_H
--- Homework 3/2 Circle/main.c
+++ Homework 3/2 Circle/main.c
@@ -1,41 +1,35 @@
 #include "circle.h"
-#include <SDL2/SDL.h>
-#include <stdio.h>
-#include<stdbool.h>
 
+#define NUM_SEGMENTS 32
 
-#define NUM_SEGMENTS 32 // A szakaszok száma
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	Circle circle;
-	double area;
-	SDL_Color color= {255,0,255,255};
-	set_circle_data(&circle, 5, 10, 8, color);
-	area = calc_circle_area(&circle);
-	
-	printf("Circle area: %lf\n", area);
+    Circle circle;
+    double area;
+    SDL_Color color = {255, 0, 255, 255};
+    set_circle_data(&circle, 5, 10, 8, color);
+    area = calc_circle_area(&circle);
 
-	SDL_Window *window = NULL;
+    printf("Circle area: %lf\n", area);
+
+    SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     bool quit = false;
     SDL_Event event;
 
-    // A kör adatai
     double radius = circle.radius;
     double centerX = 400;
     double centerY = 300;
-	SDL_SetRenderDrawColor(renderer, 255,0,255,255);
-    // Az ablak inicializálása
-	SDL_Init(SDL_INIT_VIDEO);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("Kör közelítése szakaszokkal", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    // Szakaszok rajzolása
     double angle;
     double segmentAngle = 2 * M_PI / NUM_SEGMENTS;
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // piros szín beállítása
-    for (int i = 0; i < NUM_SEGMENTS; i++) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (int i = 0; i < NUM_SEGMENTS; i++)
+    {
         angle = i * segmentAngle;
         double x1 = centerX + radius * cos(angle);
         double y1 = centerY + radius * sin(angle);
@@ -44,18 +38,17 @@ int main(int argc, char* argv[])
         SDL_RenderDrawLine(renderer, (int)x1, (int)y1, (int)x2, (int)y2);
     }
 
-    // Az ablak megjelenítése
     SDL_RenderPresent(renderer);
 
-    // Ablak bezárása eseményre
-    while (!quit) {
+    while (!quit)
+    {
         SDL_WaitEvent(&event);
-        if (event.type == SDL_QUIT) {
+        if (event.type == SDL_QUIT)
+        {
             quit = true;
         }
     }
 
-    // Ablak lezárása és SDL leállítása
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
