791830015434659942333a15b646d7fa14529efd
patak <90549150+nsbalint@users.noreply.github.com>
2024-05-31 14:17:09 +0200

    Update app.c

--- Beadandó/src/app.c
+++ Beadandó/src/app.c
@@ -16,7 +16,7 @@ void init_app(App *app, int width, int height)
     }
 
     app->window = SDL_CreateWindow(
-        "Diamond",
+        "PenguinGame",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         width, height,
         SDL_WINDOW_OPENGL);
